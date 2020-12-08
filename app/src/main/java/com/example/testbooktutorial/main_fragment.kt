ec1383641afed338fdddc624aa47cf5fd40f77ed
Dheeraj Pant <dheerajpant35@gmail.com>
2020-12-08 20:40:46 +0530

    Implemented Databinding and Fragments

--- app/src/main/java/com/example/testbooktutorial/main_fragment.kt
+++ app/src/main/java/com/example/testbooktutorial/main_fragment.kt
@@ -6,9 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 
 class main_fragment : Fragment() {
 
